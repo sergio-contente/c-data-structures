55c31b60fc534b5c9ee2c8876610d0114fcb87fa
sergio-contente <sergio.mcontente@gmail.com>
2023-08-26 18:34:26 +0200

    ✨ New recursive insertion sort

--- Sorting Algorithms/Insertion-Sort/insertion-sort.c
+++ Sorting Algorithms/Insertion-Sort/insertion-sort.c
@@ -10,8 +10,8 @@ void showMenu() {
 	printf("============   Made by:            =====\n");
 	printf("===   Sergio Contente              =====\n");
 	printf("========================================\n");
-	printf("=== 1) Insertion Sort              =====\n");
-	printf("=== 2) Simple Insertion Sort       =====\n");
+	printf("=== 1) Recursive Insertion Sort    =====\n");
+	printf("=== 2) Insertion Sort              =====\n");
 	printf("=== 3) Descreasing Insertion Sort  =====\n");
 	printf("=== 4) Reverse Insertion Sort      =====\n");
 }
@@ -26,31 +26,30 @@ void printArray(int* array, int numbersCount){
 	printf("]\n");
 }
 
-void insertionSort(int* array, int numbersCount) {
-
-	int candidate;
-	int rightArrow, leftArrow, pivot;
-
-	for (pivot = 1; pivot < numbersCount; pivot++)
+void RecursiveInsertionSort(int* array, int numbersCount, int counter) {
+	if (numbersCount < 0)
 	{
-		printArray(array, numbersCount);
-		candidate = array[pivot];
-		leftArrow = pivot - 1;
-		rightArrow = pivot;
-		while (leftArrow >= 0 && array[leftArrow] > candidate)
-		{
-			rightArrow = leftArrow + 1;
-    	array[rightArrow] = array[leftArrow];
-    	leftArrow--;
-			rightArrow--;
-		}
-		array[rightArrow] = candidate;
+		return;
 	}
+	numbersCount--;
+	counter++;
+	RecursiveInsertionSort(array, numbersCount, counter);
+	int candidate = array[numbersCount];
+	printf("Recursion step number %d\nCandidate: %d\n", numbersCount, candidate);
 	printArray(array, numbersCount);
-
+	int leftArrow = numbersCount - 2;
+	
+	while (leftArrow >= 0 && array[leftArrow] > candidate)
+	{
+		array[leftArrow + 1] = array[leftArrow];
+		leftArrow--;
+		printArray(array, numbersCount);
+	}
+	array[leftArrow + 1] = candidate;
+	printArray(array, numbersCount);
 }
 
-void SimpleInsertionSort(int* array, int numbersCount) {
+void InsertionSort(int* array, int numbersCount) {
 
 	int candidate;
 	int rightArrow, leftArrow;
@@ -113,10 +112,13 @@ int main(int argc, char* argv[]){
 	int* array;
 	int inputNumber;
 	int user_input;
+	int counter;
 
 	showMenu();
 	printf("\nInput: ");
 	scanf("%d", &user_input);
+	printf("========================================\n");
+
 
 	numbersCount = argc - 1; // Removes the name of the program argument
 	array = (int*)malloc(sizeof(int*)*numbersCount);
@@ -127,10 +129,11 @@ int main(int argc, char* argv[]){
 	switch (user_input)
 	{
 	case 1:
-		insertionSort(array, numbersCount);
+		counter = 0;
+		RecursiveInsertionSort(array, numbersCount, counter);
 		break;
 	case 2:
-		SimpleInsertionSort(array, numbersCount);
+		InsertionSort(array, numbersCount);
 		break;
 	case 3:
 		DecreaseInsertionSort(array, numbersCount);
@@ -139,7 +142,7 @@ int main(int argc, char* argv[]){
 		BackwardsInsertionSort(array,numbersCount);
 		break;
 	default:
-		SimpleInsertionSort(array, numbersCount);
+		InsertionSort(array, numbersCount);
 		break;
 	}
 	return 0;
