e4c6591cdc2ab4f972a0b376e243dfb96cfbea14
sergio-contente <sergio.mcontente@gmail.com>
2023-08-26 20:05:39 +0200

    🐛 Fixed iteractive way bug

--- Binary-Search/binary-search.c
+++ Binary-Search/binary-search.c
@@ -7,14 +7,18 @@ typedef int bool;
 #define FALSE 0
 
 void printMenu(){
-	printf("===========================\n");
-	printf("===   Binary Search   =====\n");
-	printf("===========================\n");
-	printf("***************************\n");
-	printf("===========================\n");
-	printf("===   Made by:        =====\n");
-	printf("===   Sergio Contente =====\n");
-	printf("===========================\n");
+	printf("========================================\n");
+	printf("============  Binary Search    =========\n");
+	printf("========================================\n");
+	printf("****************************************\n");
+	printf("========================================\n");
+	printf("============   Made by:            =====\n");
+	printf("===   Sergio Contente              =====\n");
+	printf("========================================\n");
+	printf("=== 1) Recursive Binary Search     =====\n");
+	printf("=== 2) Iteractive Binary Search    =====\n");
+	printf("========================================\n");
+
 }
 
 void RecursiveBinarySearch(int* array, int value, int left, int right, bool foundValue){
@@ -26,7 +30,7 @@ void RecursiveBinarySearch(int* array, int value, int left, int right, bool foun
 		{
 			if (array[left] == value)
 			{
-				printf("Value %d found! It's in the %d element of the ordered array.", value, left + 1);
+				printf("Value %d found! It's the %d element of the ordered array.", value, left + 1);
 				foundValue = TRUE;
 				return;
 			} else {
@@ -36,7 +40,7 @@ void RecursiveBinarySearch(int* array, int value, int left, int right, bool foun
 		} else {
 				mid = (left + right)/2;
 				if (array[mid] == value) {
-					printf("Value %d found! It's in the %d element of the ordered array.", value, mid + 1);
+					printf("Value %d found! It's the %d element of the ordered array.", value, mid + 1);
 					foundValue = TRUE;
 					return;
 				} 
@@ -48,19 +52,44 @@ void RecursiveBinarySearch(int* array, int value, int left, int right, bool foun
 				}
 			}
 	}
-	
-	
 }
+
+void IterativeBinarySearch(int* array, int numbersCount, int value) {
+	int left = 0;
+	int right = numbersCount - 1;
+	int mid;
+	while (left <= right)
+	{
+		mid = (left + right)/2;
+		if (array[mid] == value)
+		{
+			printf("Value %d found! It's the %d element of the ordered array.", value, mid + 1);
+			return;
+		} else {
+				if (value < array[mid])
+				{
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+		}
+	}
+
+	printf("O valor %d is not inside of the elements array.", value);
+}	
+	
 
 
 int main(int argc, char* argv[]){
-	int user_input, numbersCount, inputNumber;
+	int user_input, selection_input, numbersCount, inputNumber;
 	int* array;
 	printMenu();
 	printf("\nInput: ");
+	scanf("%d", &selection_input);
+	printf("========================================\n");
+	printf("Value: ");
 	scanf("%d", &user_input);
 	printf("========================================\n");
-
 
 	numbersCount = argc - 1; // Removes the name of the program argument
 	array = (int*)malloc(sizeof(int*)*numbersCount);
@@ -69,6 +98,17 @@ int main(int argc, char* argv[]){
 		array[i] = inputNumber;
 	}
 	mergeSort(array, 0, numbersCount - 1);
-	RecursiveBinarySearch(array, user_input, 0, numbersCount - 1, FALSE);
+	switch (selection_input)
+	{
+	case 1:
+		RecursiveBinarySearch(array, user_input, 0, numbersCount - 1, FALSE);
+		break;
+	case 2:
+		IterativeBinarySearch(array, numbersCount, user_input);
+		break;
+	default:
+		RecursiveBinarySearch(array, user_input, 0, numbersCount - 1, FALSE);
+		break;
+	}
 	return 0;
 }
