50760f57dec4af20915f646635aebd8bfa80dd0d
sergio-contente <sergio.mcontente@gmail.com>
2023-08-26 17:51:32 +0200

    🚀  Finished Merge Sort

--- Sorting Algorithms/Merge-Sort/merge-sort.c
+++ Sorting Algorithms/Merge-Sort/merge-sort.c
@@ -22,77 +22,98 @@ void printArray(int* array, int numbersCount){
 	printf("]\n");
 }
 
-void getUserArray(int* array, int numbersCount, char* argv[]){
-	int inputNumber;
-	array = (int*)malloc(sizeof(int*)*numbersCount);
-	for(int i = 0; i < numbersCount; i++) {
-		inputNumber = atoi(argv[i + 1]); // argv[0] is the name of the program
-		array[i] = inputNumber;
-	}
-	printArray(array, numbersCount);
-}
-
 void merge(int* array, int left, int middle, int right){
-	int start_left = left;
-	int start_right = middle + 1;
-	int start_aux = 0;
-	int lenght_aux = right - left + 1;
+	printf("Left: %d  Middle: %d  Right: %d\n", left, middle, right);
+	int arrowLeft, arrowRight, arrowMerge = left;
+	int length_left = middle - left + 1;
+	int length_right = right - middle;
 
-	int* arrayAux;
+	int* leftArray; 
+	int* rightArray;
+	leftArray = (int*)malloc(sizeof(int*) * length_left);
+	rightArray = (int*)malloc(sizeof(int*) * length_right);
 
-	arrayAux = (int*)malloc(lenght_aux * sizeof(int));
-
-	while (start_left <= middle && start_right <= right) {
-		if(array[start_left] < array[start_right]){
-			arrayAux[start_aux] = array[start_left];
-			start_left++;
-		} else {
-			arrayAux[start_aux] = array[start_right];
-			start_right++;
-		}
-		start_aux++;
-	}
-
-	while (start_left <= middle)
+	//Creating the two arrays that will be merged later
+	for (arrowLeft = 0; arrowLeft < length_left; arrowLeft++)
 	{
-		arrayAux[start_aux] = array[start_left];
-		start_aux++;
-		start_left++;
-	}
-
-	while (start_right <= right)
-	{
-		arrayAux[start_aux] = array[start_right];
-		start_aux++;
-		start_right;
+		leftArray[arrowLeft] = array[arrowLeft + left]; // Filling the left sorted array
 	}
 	
-	for(start_aux = left; start_aux <= right; start_aux++) {
-		array[start_aux] = arrayAux[start_aux - left];
+	for (arrowRight = 0; arrowRight < length_right; arrowRight++)
+	{
+		rightArray[arrowRight] = array[arrowRight + middle + 1]; // Filling the right sorted array
 	}
+	
+	printf("\nArray left: ");
+	printArray(leftArray, length_left);
 
-	printArray(array, lenght_aux);
-	free(arrayAux);
+	printf("\nArray right: ");
+	printArray(rightArray, length_right);
+
+	// Merge loop
+	arrowLeft = 0;
+	arrowRight = 0;
+	while (arrowLeft < length_left && arrowRight < length_right)
+	{
+		if (leftArray[arrowLeft] <= rightArray[arrowRight])
+		{
+			array[arrowMerge] = leftArray[arrowLeft];
+			arrowLeft++;
+		} else {
+			array[arrowMerge] = rightArray[arrowRight];
+			arrowRight++;
+		}
+		arrowMerge++;
+	}
+	
+	// If one of the arrays finished before the other,
+	// put the rest of the elements on the merged array
+
+	while (arrowLeft < length_left)
+	{
+		array[arrowMerge] = leftArray[arrowLeft];
+		arrowLeft++;
+		arrowMerge++;
+	}
+	
+	while (arrowRight < length_right)
+	{
+		array[arrowMerge] = rightArray[arrowRight];
+		arrowRight++;
+		arrowMerge++;
+	}
+	
+	//clean memory spaces of the auxiliary arrays
+	free(leftArray);
+	free(rightArray);
 
 }
 
 void mergeSort(int* array, int left, int right) {
 	if (left < right)
 	{
+		printArray(array, left + right - 1);	
 		int middle = (left+right)/2;
 
 		mergeSort(array, left, middle);
 		mergeSort(array, middle+1, right);
 		merge(array, left, middle, right);
+		printArray(array, right - left + 1);
 	}
 }
 
 int main(int argc, char* argv[]) {
 	int* array;
 	int numbersCount = argc - 1; // Removes the name of the program argument;
-
+	int inputNumber;
  	showMenu();
-	getUserArray(array, numbersCount, argv);
-	mergeSort(array, 0, 4);
+
+	numbersCount = argc - 1; // Removes the name of the program argument
+	array = (int*)malloc(sizeof(int*)*numbersCount);
+	for(int i = 0; i < numbersCount; i++) {
+		inputNumber = atoi(argv[i + 1]); // argv[0] is the name of the program
+		array[i] = inputNumber;
+	}
+	mergeSort(array, 0, numbersCount - 1);
 	return 0;
 }
